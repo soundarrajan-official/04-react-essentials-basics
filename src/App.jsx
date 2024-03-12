e6c3c5b67cb7cfc10dce8e02f59ccace538d4792
soundarrajan-official <soundarrajan.official@gmail.com>
2024-03-12 19:04:40 +0530

    Create React Essentials App Initial Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 function App() {
   return (
     <div>
-        <p>Hello React + Vite</p>
+        <header>
+            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+            <h1>React Essentials</h1>
+            <p>
+                Fundamental React concepts you will need for almost any app you are going to build
+            </p>
+        </header>
+        <main>
+            <h2>Time to get started!</h2>
+        </main>
     </div>
   )
 }
