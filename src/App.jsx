20da55fc98a80f52efbc930d81bcc69d71f6f6e2
soundarrajan-official <soundarrajan.official@gmail.com>
2024-03-13 14:59:13 +0530

    Create and use the reusable CoreConcepts Custom Component by configuring Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import reactImage from "./assets/react-core-concepts.png"
+import componentsImage from "./assets/components.png"
+import jsxImage from "./assets/jsx-ui.png"
+import propsImage from "./assets/config.png"
+import stateImage from "./assets/state-mgmt.png"
 
 const reactDescriptions = ["Fundamentals", "Crucial", "Core"]
 
@@ -21,12 +25,46 @@ function Header() {
     )
 }
 
+function CoreConcepts(props) {
+    return (
+        <li>
+            <img src={props.image} alt={props.title}/>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    )
+}
+
 function App() {
     return (
         <div>
             <Header />
             <main>
-                <h2>Time to get started!</h2>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcepts
+                            title="Components"
+                            description="The Core UI building Block"
+                            image={componentsImage}
+                        />
+                        <CoreConcepts
+                            title="JSX"
+                            description="Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered"
+                            image={jsxImage}
+                        />
+                        <CoreConcepts
+                            title="Props"
+                            description="Make components configurable (and therefore reusable) by passing input data to them"
+                            image={propsImage}
+                        />
+                        <CoreConcepts
+                            title="State"
+                            description="React-managed data which, when changed, caused the component to re-render and the UI to update"
+                            image={stateImage}
+                        />
+                    </ul>
+                </section>
             </main>
         </div>
     )
